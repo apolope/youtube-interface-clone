884ef51f7ac5b0d645102914571dbd8dc3661795
Apolonio Serafim <apolonio.serafim@hotmail.com>
2021-03-02 19:14:17 -0300

    Material UI configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Button } from "@material-ui/core";
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>      
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <Button variant="outlined" color="secondary">Hello World!</Button>
     </div>
   )
 }
